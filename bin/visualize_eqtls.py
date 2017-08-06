49d2de354d05cb17739451b3ed52b930b05706a2
Brian Hie <brianhie@stanford.edu>
2017-08-06 15:28:22 -0700

    Added meta data functionality

--- bin/visualize_eqtls.py
+++ bin/visualize_eqtls.py
@@ -1,3 +1,4 @@
+import json
 from math import log
 import numpy as np
 import sys
@@ -5,26 +6,48 @@ import pickle
 
 from tsne import tsne
 
+P_VAL_CUTOFF = 0.0001
+
 if __name__ == '__main__':
+    # Open a dictionary from QTL locations to a list of p-values
+    # corresponding to different phenotypes.
     with open(sys.argv[1], 'rb') as p_file:
         qtl_to_p = pickle.load(p_file)
 
-    for qtl in sorted(qtl_to_p.keys()):
-        print('> {0}:{1}'.format(qtl[0], qtl[1]))
+    # Get the namespaces that correspond to the joined p-values.
+    conf_name = sys.argv[1].split('/')[-1].split('.')[0]
+    if conf_name.startswith('eqtls_'):
+        conf_name = conf_name[len('eqtls_'):]
+    with open('conf/{}.json'.format(conf_name), 'r') as conf_file:
+        joined_namespaces = json.loads(conf_file.read())
 
-    exit()
-
+    # Initialize the matrix of the number of QTLs by the number of QTL
+    # phenotypes.
     n_qtl_types = len(next(iter(qtl_to_p.values())))
     X = np.zeros((
         len(qtl_to_p), n_qtl_types
     ))
     for row, qtl in enumerate(sorted(qtl_to_p.keys())):
+        # Fill in the matrix.
         X[row, :] = qtl_to_p[qtl]
-#        X[row, :] = [ -log(q) if q != 0 else 308
-#                      for q in qtl_to_p[qtl] ]
 
+        # Label a SNP as a QTL if the p-value is less than some arbitrary
+        # value.
+        labels = []
+        for pos, q in enumerate(qtl_to_p[qtl]):
+            if q < P_VAL_CUTOFF:
+                labels.append(joined_namespaces[pos])
+                
+        # Print to stdout, which can be used as metadata file for the
+        # visualization.
+        print('> {0}:{1} {2}'.format(qtl[0], qtl[1], labels))
+
+    exit()
+
+    # Visualize the 3D space after doing a t-SNE dimensionality reduction.
     Y = tsne(X, no_dims=3, initial_dims=n_qtl_types)
 
+    # Save the t-SNE projecte coordinates to a file.
     name = sys.argv[1].split('/')[-1]
     if name.endswith('.pickle'):
         name = name[:-len('.pickle')]
